2b9c959a4d41c2ac54e251f73c6dfbf98fabb2e6
KenwoodFox <kenwood364@gmail.com>
2022-03-18 22:20:51 -0400

    Flesh out the rest of the operator module

--- arduino/mds/lib/operator/operator.cpp
+++ arduino/mds/lib/operator/operator.cpp
@@ -13,8 +13,11 @@ Operator::Operator(int _throttlePin, int _steerPin, int _autonomousPin, int _rec
 
     // Pins
     throttlePin = _throttlePin;
+    pinMode(throttlePin, INPUT);
     steerPin = _steerPin;
+    pinMode(steerPin, INPUT);
     autonomousPin = _autonomousPin;
+    pinMode(autonomousPin, INPUT);
 
     // PWM values
     recvMin = _recvMin;
@@ -23,16 +26,36 @@ Operator::Operator(int _throttlePin, int _steerPin, int _autonomousPin, int _rec
 
 bool Operator::getManualOverride() const
 {
-    return false;
+    int _ret = pulseIn(throttlePin, HIGH);
+
+    if (_ret > 1500)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
 };
 
 int Operator::getThrottle() const
 {
     // Note: this is a bad way to do this, it halts the CPU, a better way would be to use interrupts.
-    int _ret = pulseIn(3, HIGH);
+    int _ret = pulseIn(throttlePin, HIGH);
 
     // Map ret between -128 and 128
-    _ret = map(_ret, 1100, 1800, -128, 128);
+    _ret = map(_ret, recvMin, recvMax, -128, 128);
+
+    return _ret;
+};
+
+int Operator::getSteer() const
+{
+    // Note: this is a bad way to do this, it halts the CPU, a better way would be to use interrupts.
+    int _ret = pulseIn(steerPin, HIGH);
+
+    // Map ret between -128 and 128
+    _ret = map(_ret, recvMin, recvMax, -128, 128);
 
     return _ret;
 };
--- arduino/mds/lib/operator/operator.h
+++ arduino/mds/lib/operator/operator.h
@@ -12,8 +12,9 @@ private:
     int recvMax;
 
 public:
-    Operator(int _throttlePin, int _steerPin, int _autonomousPin, int _recvMin, int _recvMax);
+    Operator(int _throttlePin, int _steerPin, int _autonomousPin, int _recvMin = 1100, int _recvMax = 1800);
 
     bool getManualOverride() const;
     int getThrottle() const;
+    int getSteer() const;
 };
